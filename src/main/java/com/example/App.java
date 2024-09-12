529810765b5a50c245c06e73e6b153e5fc11a231
ilya <ilya_zhenetskij@vk.com>
2024-09-12 18:36:10 +0700

    z1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,8 +6,21 @@ package com.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static String fizzbuzz(int n) {
+        if (n % 5 == 0 && n % 7 == 0) {
+            return "fizzbuzz";
+        } else if (n % 5 == 0) {
+            return "fizz";
+        } else if (n % 7 == 0) {
+            return "buzz";
+        } else {
+            return Integer.toString(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 500; i++) {
+            System.out.println(fizzbuzz(i));
+        }
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -3,10 +3,18 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class AppTest {
 
     @Test
-    void testAddition() {
-        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    public void testFizzbuzz() {
+        // Тестируем конкретные значения
+        assertEquals("1", App.fizzbuzz(1)); // Ожидаем 1
+        assertEquals("fizz", App.fizzbuzz(5)); // Ожидаем fizz
+        assertEquals("buzz", App.fizzbuzz(7)); // Ожидаем buzz
+        assertEquals("fizzbuzz", App.fizzbuzz(35)); // Ожидаем fizzbuzz
+        assertEquals("fizz", App.fizzbuzz(10)); // Ожидаем fizz
+        assertEquals("buzz", App.fizzbuzz(49)); // Ожидаем buzz
+        assertEquals("17", App.fizzbuzz(17)); // Ожидаем 17
     }
 }
